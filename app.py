988e6409c8393d1b82fdad0b87dd94f4ef5b0802
Naufal Luthfan <an4667112@gmail.com>
2026-06-01 09:27:19 +0700

    Remove copyright notice from footer
    Removed copyright notice from footer.

--- app.py
+++ app.py
@@ -212,7 +212,6 @@ st.markdown(
         by 
         <a href="https://github.com/Naufallm" target="_blank" style="color: #238636; text-decoration: none; font-weight: bold;">Naufallm</a> & 
         <a href="https://github.com/syahrialfaturr" target="_blank" style="color: #238636; text-decoration: none; font-weight: bold;">syahrialfaturr</a> 
-        | © 2026 IDX AI Research Engine
     </div>
     """,
     unsafe_allow_html=True
